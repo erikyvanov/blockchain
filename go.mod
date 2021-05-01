0995667152da2b47bef1d7e4e3090a00356e0772
Erik Ivanov <erik.ivanov.n@gmail.com>
2021-05-01 06:38:43 -0500

    base

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/erikyvanov
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{
+		Timestamp:     time.Now().Unix(),
+		Data:          []byte(data),
+		PrevBlockHash: prevBlockHash,
+		Hash:          []byte{},
+	}
+
+	block.SetHash()
+
+	return block
+}
+
+func main() {
+	b := NewBlock("Mi primer block", []byte("34fd33"))
+	fmt.Println(string(b.Data))
+	fmt.Println(string(b.Hash))
+	fmt.Println(string(b.PrevBlockHash))
+	fmt.Println(b.Timestamp)
+}
